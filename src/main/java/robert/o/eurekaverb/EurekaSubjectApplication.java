b4fde6a2aeac670288ae24c0649d7e13ccbddc3c
robertoelerojunior <robertoelerojunior@gmail.com>
2018-12-17 21:35:22 -0200

    First commit.

---
+++ src/main/java/robert/o/eurekaverb/EurekaSubjectApplication.java
@@ -0,0 +1,14 @@
+package robert.o.eurekaverb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaSubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSubjectApplication.class, args);
+	}
+}
